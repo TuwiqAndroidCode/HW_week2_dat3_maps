1432a6472ea8a4c5f47474c95836f06cd52b293c
Rahaf Nasser <rahaf.nasser.ruh.k@tuwaiq.edu.sa>
2021-10-19 21:03:20 +0300

    week2 day3 maps/Rahaf Nasser

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
 fun main() {
     //Create a map with 4 elements with add, edit and remove
-    var myMap= mutableMapOf(1 to "first", 2 to "second", 3 to "third", 4 to "forth")
+    var myMap = mutableMapOf(1 to "first", 2 to "second", 3 to "third", 4 to "forth")
     //println(myMap)
     //add
-    myMap[5]="fifth"
+    myMap[5] = "fifth"
     //println(myMap)
     //edit
-    myMap[1]="1st"
+    myMap[1] = "1st"
     //println(myMap)
     //remove
     myMap.remove(4)
@@ -14,19 +14,19 @@ fun main() {
 
     println("-------------------------")
     //Create a map of 3 elemnts and create 3 elements of list add the list to the map
-    var myMap2= mutableMapOf(1 to "first", 2 to "second", 3 to "third")
+    var myMap2 = mutableMapOf(1 to "first", 2 to "second", 3 to "third")
     //println(myMap2)
-    myMap2.putAll(listOf(4,5,6).mapIndexed(){key,element-> key+4 to element.toString()})
+    myMap2.putAll(listOf(4, 5, 6).mapIndexed() { key, element -> key + 4 to element.toString() })
     //println(myMap2)
 
     println("-------------------------")
     //ENG. Mohamed's question
     //Multiplication
 
-    var input=5
-    for(count1 in 1..input){
-        for (count2 in 1..count1){
-            print("$count2"+"x$input=${input*count2}\t")
+    var input = 5
+    for (count1 in 1..input) {
+        for (count2 in 1..count1) {
+            print("$count2" + "x$input=${input * count2}\t")
         }
         println()
     }
