3190f10a01fe6e3f439d9d162ac18f5de30c5d60
Nora <Norah.Nahedh.ruh.k@tuwaiq.edu.sa>
2021-10-19 19:46:11 +0300

    HW done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,25 @@
 fun main(args: Array<String>) {
 
 
-// your code here
+// Part I
+val NorasFavoriteStuff = mutableMapOf("drama" to "vincenzo the mafia man" ,
+    "animation" to "attack on titan",
+    "animal" to "cat",
+    "bff" to "khloud")
+println(NorasFavoriteStuff)
+//add new element
+    NorasFavoriteStuff["Qusine"]= "italian"
+    //edit
+    NorasFavoriteStuff.put("drama" , "are you a human too")
 
+    //delete
+    NorasFavoriteStuff.remove("Qusine")
+     println(NorasFavoriteStuff)
 
+  //Part II
 
+    val toWatch = mutableMapOf(1 to "one woman" , 2 to "The Veil" , 3 to "Dali And Cocky Prince")
+    //add list
+    toWatch.putAll(listOf(4 to "kill it", 5 to "Hometown Cha-Cha-Cha" , 6 to "lost"))
+    println(toWatch)
 }
