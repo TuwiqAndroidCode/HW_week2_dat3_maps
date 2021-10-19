2453969c8cd6cb31e8ad20e231bcf8085d047847
lamia <Lamia.Omar.ruh.k@tuwaiq.edu.sa>
2021-10-19 20:14:13 +0300

    HW_week2_dat3_map.iml

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,26 @@
 fun main(args: Array<String>) {
+//part|
+var mapLamia = mutableMapOf<String,String>(
+    "In1" to "Lamia" , "In2" to "Omar" , "In3" to "Huda" , "In4" to "Shayam" )
+    println(mapLamia)
+//ADD
+    mapLamia.put("In5" , "Ghaida")
+    println(mapLamia)
+//EDIT
+    mapLamia["In4"]="Maryam"
+    println(mapLamia)
+//remove
+    mapLamia.remove("In5")
+    println(mapLamia)
 
+    println("----------------------------------")
 
-// your code here
+//part||
+    var mapList = mutableMapOf<String,String>(
+        "Inst1" to "Kotlin" , "Inst2" to "Java" , "Inst3" to "C++")
+    println(mapList)
+    mapList.putAll(listOf("Swift", "PHP" , "Python").mapIndexed({x, y -> x.toString() to y }))
+    println(mapList)
 
 
 
