79f490b45ee7d55bf06ef6124493c1eb25bec9da
khalid <kaled1418g@gmai.com>
2021-10-19 21:26:54 +0300

    H.W map and Bons question

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,44 @@ fun main(args: Array<String>) {
 
 
 // your code here
+//H.W part1
+
+
+    var mymap= mutableMapOf("door" to "key" , "car" to "drive" , "hand" to "catch" , "leg" to "move")
+    mymap["book"] = "read"
+    println(mymap)
+    mymap["door"] = "lock"
+    println(mymap)
+    mymap.remove("leg")
+    println(mymap)
+
+
+
+    //=====================
+    //part2
+    var newmap = mutableMapOf(1 to "khalid" , 2  to "zamel" , 3 to "hussain")
+    println(newmap)
+    newmap.putAll(listOf("aymen", "rakan", "zaid").mapIndexed{index,name -> index.toInt()to name})
+    println(newmap)
+    newmap.putAll(mapOf(4 to "aymen" , 5 to "rakan" , 6 to "zaid"))
+    println(newmap)
+    newmap.remove(0)
+    println(newmap)
+
+
+    //bons question
+
+
+    var z = 5
+    for (y in 1..z){
+        for (x in 1..y){
+            print("$x*$y ")
+        }
+        println()
+    }
+
+
+
 
 
 
