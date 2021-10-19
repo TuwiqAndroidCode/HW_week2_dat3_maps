f90166da111c37199ab3494c6bfc02fd971f0503
Reem Altammami <reem.saud.ruh.k@tuwaiq.edu.sa>
2021-10-19 22:45:26 +0300

    part1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
 fun main(args: Array<String>) {
 
 
-// your code here
+// *** Part 1 ***
+
+    var numbersMap=mutableMapOf(1 to "First",2 to "Second",3 to "Third",4 to "Fourth" )
+    numbersMap.put(5 , "Fifth")
+    numbersMap.putIfAbsent(6,"Sixth")
+    numbersMap.putAll(mapOf(7 to "Seventh",8 to "Eighth"))
+    numbersMap[1]="1st"
+    numbersMap.replace(2,"2nd")
+    numbersMap.remove(8)
+    println(numbersMap)
 
 
 
