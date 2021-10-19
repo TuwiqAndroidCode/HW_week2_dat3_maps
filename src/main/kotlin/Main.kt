3bba114f5de7ed082514429b9808564d1f11ec15
haya <haya.saud.ruh.k@tuwaiq.edu.sa>
2021-10-19 23:14:06 +0300

    19 OCT homework and optional question

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,44 @@
 fun main(args: Array<String>) {
 
+        //ex1===========================================================================================
+/*
+    var myMap = mutableMapOf("haya" to "alrawdhan","hessah" to "alsaad", "reem" to "alhajri")//create map
+    println(myMap)
+    myMap.putIfAbsent("raghad","alghamdi") //add element
+    println(myMap)
+    myMap["hessah"]="afrah" //edit element
+    println(myMap)
+    myMap.remove("hessah") //remove element
+    println(myMap)
+    */
 
-// your code here
+
+        //EX2=============================================================================================
+
+        /*  var familyMap = mutableMapOf("father" to "saud","mather" to "jameelah", "sun" to "abdullah")//create map
+          familyMap.putAll(listOf("reem" , "ali", "abdulrahman").mapIndexed{index, s -> index.toString() to s }) //create list 3 element and add it to map
+          println(familyMap)*/
 
 
 
-}
+
+        //option qustion====================================================================================:)
+
+        var n=5
+        var x=1
+
+        while (x <= n) {
+            var y=1
+            while (y<=x){
+
+                print("$y*5 ")
+                y++
+            }
+            println()
+            x++
+        }
+
+    }
+
+
+
