1bd3adc6f566bc325c934182064e2e3b07b3aba7
abdullah <Abdullah.Mohammed.ruh.k@tuwaiq.edu.sa>
2021-10-20 00:27:56 +0300

    HW MAP PARI I II ABDULLAH ALSMARI

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,18 @@
-fun main(args: Array<String>) {
-
-
-// your code here
+fun main (args: Array<String>)  {
 
 
 
-}
+// ============= part I ==============
+        val infoMap = mutableMapOf("Name" to "Abdullah", "SeName" to "Mohammed", "LaName" to "ALSMARI")
+        infoMap.put("ID", "12903")
+        println(infoMap)
+        infoMap ["ID"]="100000"
+        infoMap.remove("SeName","Mohammed")
+        println(infoMap)
+// ============= part II ==============
+        val  MyFamily =mutableMapOf("Mam" to "Sara","Dad" to "Mohammed" , "BigBrother" to "Salman")
+        MyFamily.putAll(listOf("Me" to "Abdullah","Sister" to "Nada","Turtle" to "emmy"))
+        println(MyFamily)
+
+    }
+
