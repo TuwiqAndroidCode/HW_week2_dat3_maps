d2b70a4f6a23f6fb61d029c54e088e0bee50427c
raghad <RAGHAD.MOHAMMED.ruh.k@tuwaiq.edu.sa>
2021-10-20 08:32:41 +0300

    add exstra question solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -23,4 +23,55 @@ fun main(args: Array<String>) {
 
     println(secondMap)
 
+
+
+println("--------------------")
+
+//Exstra Question
+
+
+    for (i in 1..9){
+
+        print("1 X $i = ${1 * i} \t")
+    }
+    print("\n")
+    for (i in 1..9){
+
+        print("2 X $i = ${2 * i} \t")
+    }
+    print("\n")
+    for (i in 1..9){
+
+        print("3 X $i = ${3 * i} \t")
+    }
+    print("\n")
+    for (i in 1..9){
+
+        print("4 X $i = ${4 * i} \t")
+    }
+    print("\n")
+    for (i in 1..9){
+
+        print("5 X $i = ${5 * i} \t")
+    }
+    print("\n")
+    for (i in 1..9){
+
+        print("6 X $i= ${6 * i} \t")
+    }
+    print("\n")
+    for (i in 1..9){
+
+        print("7 X $i= ${7 * i} \t")
+    }
+    print("\n")
+    for (i in 1..9){
+
+        print("8 X $i= ${8 * i} \t")
+    }
+    print("\n")
+    for (i in 1..9){
+
+        print("9 X $i = ${9*i} \t ")
+    }
 }
